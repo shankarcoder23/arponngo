b03c8c1a93e68fe4b8352065ef99446e7073e700
shankarcoder23 <shankar.coder23@gmail.com>
2026-03-27 15:05:34 +0530

    updated all

--- aseets/js/gallery.js
+++ aseets/js/gallery.js
@@ -3,30 +3,21 @@
   toggler.addEventListener("click", function () {
     this.classList.toggle("active");
   });
-  // View More
-  document.getElementById("viewMoreBtn").onclick = function () {
-    document.getElementById("fullGallery").classList.remove("d-none");
-    this.style.display = "none";
-  };
+const images = document.querySelectorAll(".gallery-img");
+const modalImage = document.getElementById("modalImage");
+const modalCaption = document.getElementById("modalCaption");
 
-  // Show Less
-  document.getElementById("showLessBtn").onclick = function () {
-    document.getElementById("fullGallery").classList.add("d-none");
-    document.getElementById("viewMoreBtn").style.display = "inline-block";
-    window.scrollTo({
-      top: document.getElementById("gallery").offsetTop - 50,
-      behavior: "smooth"
-    });
-  };
+images.forEach(img => {
+  img.addEventListener("click", function () {
 
-  // Image Click → Modal
-  document.querySelectorAll(".gallery-img").forEach(img => {
-    img.addEventListener("click", function () {
-      document.getElementById("modalImage").src = this.src;
-      document.getElementById("modalTitle").innerText = this.dataset.title;
-      document.getElementById("modalDate").innerText = this.dataset.date;
+    modalImage.src = this.src;
 
-      let modal = new bootstrap.Modal(document.getElementById("galleryModal"));
-      modal.show();
-    });
+    let title = this.getAttribute("data-title");
+    let date = this.getAttribute("data-date");
+
+    modalCaption.innerHTML = `<strong>${title}</strong><br>${date}`;
+
+    let modal = new bootstrap.Modal(document.getElementById('imageModal'));
+    modal.show();
   });
+});
